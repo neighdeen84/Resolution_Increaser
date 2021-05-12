07e6bc590d0f47e7fbcaa96ceea58f665e5b3f29
nadine <nadeen@pdx.edu>
2021-05-12 01:06:55 -0700

    finishing touches

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import pandas as pd
 import numpy as np
-#import datetime
-from datetime import datetime
-from dateutil.parser import parse
 
 # Enter in the CSVs you want to process:
-# this is for the power values:
-
-# This is purely for the timestamp, concatenate later:
-df = pd.read_csv('14545_house_profile_10_.csv', header=None)
+df = pd.read_csv('14545_house_profile_1.csv', header=None)
 
 # Give labels
 df.columns =['Timestamp', 'Power']
-
 
 # Make sure Timestamp type is datetime
 df['Timestamp'] = df['Timestamp'].astype('datetime64')
@@ -28,20 +21,20 @@ df = df.resample('300s', label='right',closed='right').mean()
 df.reset_index(level=0, inplace=True)
 
 for i in range(len(df)):
-    # Check each value for nan
+    # Check each value for nan, if so set it equal to the value before it:
     if np.isnan(df.iloc[i,1]):
         df.iloc[i,1] = df.iloc[i-1,1]
 print(df)
 
-#print(df['Timestamp'].dtypes)
+# Change the year from 2013 to 2021:
 df['Timestamp'] = df['Timestamp'].mask(df['Timestamp'].dt.year == 2013, df['Timestamp'] + pd.offsets.DateOffset(year=2021))
-#print(df)
 
 # Add missing rows to df and use last power value:
+# NOTE: this was only added because the original dataset ends at 23:45:00
 df.loc[len(df.index)] = ['2021-01-12 23:50:00', df.iloc[-1,1]]
 df.loc[len(df.index)] = ['2021-01-12 23:55:00', df.iloc[-1,1]]
 print(df)
 
 
 # Printing out sampled CSV with removed headers (col labels) and keeping the index (timestamp col):
-df.to_csv('14545_house_profile_10.csv', header=False, index=False)
+df.to_csv('14545_house_profile_1_5_mins.csv', header=False, index=False)
